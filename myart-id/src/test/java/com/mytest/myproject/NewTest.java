32de7c1f2c09ecd643fbfe8c77c56df619797d70
Taraikovsky <taraikovsky.ns@gmail.com>
2016-05-04 16:45:51 +0300

    'q'

---
+++ myart-id/src/test/java/com/mytest/myproject/NewTest.java
@@ -0,0 +1,13 @@
+package com.mytest.myproject;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	@Test
+	public void testMyFirstTest() {
+	
+		System.out.println("Hello World");
+	Assert.assertEquals(true, false);
+	}
+}
